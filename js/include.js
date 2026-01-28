035848a6f8534689bd2352276fad185f89bd0057
jayeshdeveloper <jayesh.developer546@gmail.com>
2026-01-28 17:19:24 +0530

    final commit

--- js/include.js
+++ js/include.js
@@ -1,9 +1,7 @@
 function loadComponent(id, file) {
-    fetch(file)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById(id).innerHTML = data;
-        });
+  fetch(file)
+    .then(res => res.text())
+    .then(data => document.getElementById(id).innerHTML = data);
 }
 
 loadComponent("header", "components/header.html");
